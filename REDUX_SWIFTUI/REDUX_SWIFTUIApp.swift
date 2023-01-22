d272d82cfda89f7c848da2872681c6727dbb987b
Azizbek Asadov <azizbek1998@icloud.com>
2023-01-22 22:17:08 +0500

    Initial Commit

---
+++ REDUX_SWIFTUI/REDUX_SWIFTUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  REDUX_SWIFTUIApp.swift
+//  REDUX_SWIFTUI
+//
+//  Created by Azizbek Asadov on 22/01/23.
+//
+
+import SwiftUI
+
+@main
+struct REDUX_SWIFTUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
